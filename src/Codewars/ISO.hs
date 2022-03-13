db3cd975c8fbdef03a0724ebff642c6590acff5e
Дамир <dmingazov@htc-cs.ru>
2022-03-13 19:58:44 +0400

    Resolve "Algebraic Isomorphism"

--- src/Codewars/ISO.hs
+++ src/Codewars/ISO.hs
@@ -106,3 +106,168 @@ isoEU = (fev, fve)
 -- iab -> iba, iba -> iab
 isoSymm :: ISO (ISO a b) (ISO b a)
 isoSymm = (symm, symm)
+
+
+-- https://www.codewars.com/kata/5917f22dd2563a36a200009c/train/haskell
+
+-- a = b -> c = d -> a * c = b * d
+isoProd :: ISO a b -> ISO c d -> ISO (a, c) (b, d)
+isoProd = isoTuple
+
+-- a = b -> c = d -> a + c = b + d
+isoPlus :: ISO a b -> ISO c d -> ISO (Either a c) (Either b d)
+isoPlus = isoEither
+
+-- a = b -> S a = S b
+isoS :: ISO a b -> ISO (Maybe a) (Maybe b)
+isoS = isoMaybe
+
+-- a = b -> c = d -> c ^ a = d ^ b
+isoPow :: ISO a b -> ISO c d -> ISO (a -> c) (b -> d)
+isoPow = isoFunc
+
+-- a + b = b + a
+plusComm :: ISO (Either a b) (Either b a)
+plusComm = (swap, swap)
+  where
+    swap (Right x) = Left x
+    swap (Left x) = Right x
+
+-- a + b + c = a + (b + c)
+plusAssoc :: ISO (Either (Either a b) c) (Either a (Either b c))
+plusAssoc = (func1, func2)
+  where
+    func1 :: Either (Either a b) c -> Either a (Either b c)
+    func1 (Right x) = Right $ Right x
+    func1 (Left (Right x)) = Right $ Left x
+    func1 (Left (Left x)) = Left x
+
+    func2 :: Either a (Either b c) -> Either (Either a b) c
+    func2 (Left x) = Left $ Left x
+    func2 (Right (Left x)) = Left $ Right x
+    func2 (Right (Right x)) = Right x
+
+-- a * b = b * a
+multComm :: ISO (a, b) (b, a)
+multComm = (uncurry $ flip (,), uncurry $ flip (,))
+
+-- a * b * c = a * (b * c)
+multAssoc :: ISO ((a, b), c) (a, (b, c))
+multAssoc = (func1, func2)
+  where
+    func1 ((a, b), c) = (a, (b, c))
+    func2 (a, (b, c)) = ((a, b), c)
+
+-- dist :: a * (b + c) = a * b + a * c
+dist :: ISO (a, (Either b c)) (Either (a, b) (a, c))
+dist = (func1, func2)
+  where
+    func1 (a, e) = case e of
+      Left b -> Left (a, b)
+      Right c -> Right (a, c)
+
+    func2 (Left (a, b)) = (a, Left b)
+    func2 (Right (a, c)) = (a, Right c)
+
+-- (c ^ b) ^ a = c ^ (a * b)
+curryISO :: ISO (a -> b -> c) ((a, b) -> c)
+curryISO = (uncurry, curry)
+
+-- 1 = S O (we are using peano arithmetic)
+-- https://en.wikipedia.org/wiki/Peano_axioms
+one :: ISO () (Maybe Void)
+one = (const Nothing, const ())
+
+-- 2 = S (S O)
+two :: ISO Bool (Maybe (Maybe Void))
+two = (func1, func2)
+  where
+    func1 True = Just Nothing
+    func1 False = Nothing
+
+    func2 Nothing = False
+    func2 (Just _) = True
+
+
+-- O + b = b
+plusO :: ISO (Either Void b) b
+plusO = (left, Right)
+  where
+    left (Left  x) = absurd x -- absurd :: Void -> a
+    left (Right x) = x
+
+-- S a + b = S (a + b)
+plusS :: ISO (Either (Maybe a) b) (Maybe (Either a b))
+plusS = (func1, func2)
+  where
+    func1 (Right b) = Just $ Right b
+    func1 (Left Nothing) = Nothing
+    func1 (Left (Just a)) = Just $ Left a
+
+    func2 Nothing = Left Nothing
+    func2 (Just (Left a)) = Left $ Just a
+    func2 (Just (Right b)) = Right b
+
+-- 1 + b = S b
+plusSO :: ISO (Either () b) (Maybe b)
+plusSO = isoPlus one refl `trans` plusS `trans` isoS plusO
+
+-- O * a = O
+multO :: ISO (Void, a) Void
+multO = (fst, absurd)
+
+-- S a * b = b + a * b
+multS :: ISO (Maybe a, b) (Either b (a, b))
+multS = (func1, func2)
+  where
+    func1 (Nothing, b) = Left b
+    func1 (Just a, b) = Right (a, b)
+
+    func2 (Left b) = (Nothing, b)
+    func2 (Right (a, b)) = (Just a, b)
+
+-- 1 * b = b
+multSO :: ISO ((), b) b
+multSO =
+  isoProd one refl `trans`
+    multS `trans`
+    isoPlus refl multO `trans`
+    plusComm `trans`
+    plusO
+
+-- a ^ O = 1
+powO :: ISO (Void -> a) ()
+powO = (const (), const absurd)
+
+-- a ^ (S b) = a * (a ^ b)
+powS :: ISO (Maybe b -> a) (a, b -> a)
+powS = (func1, func2)
+  where
+    func1 fmb = (fmb Nothing, fmb . Just)
+    func2 (a, f) Nothing = a
+    func2 (a, f) (Just b) = f b
+
+-- a ^ 1 = a
+-- Go the hard way (like multSO, plusSO)
+-- to prove that you really get what is going on!
+powSO :: ISO (() -> a) a
+powSO =
+  isoPow one refl `trans`
+    powS `trans`
+    isoProd refl powO `trans`
+    multComm `trans`
+    multSO
+
+-- Here's a trick: 
+-- replace undefined with the rhs of the comment on previous line
+-- When you're not sure what to fill in for a value,
+-- Have it as a _
+-- GHC will goes like
+-- "Found hole `_' with type: ISO (() -> a) (Maybe b0 -> a0)"
+-- So you can immediately see value of what type are needed
+-- This process can be repeat indefinitely -
+-- For example you might replace `_` with `isoFunc _ _`
+-- So GHC hint you on more specific type.
+-- This is especially usefull if you have complex type.
+-- See https://wiki.haskell.org/GHC/Typed_holes
+-- And "stepwise refinement" for more details.
--- test/unit/Codewars/ISOSpec.hs
+++ test/unit/Codewars/ISOSpec.hs
@@ -12,6 +12,17 @@ bISO = (not, not)
 lrl :: ISO a b -> (a -> a)
 lrl (ab, ba) = ba . ab
 
+rlr :: ISO a b -> (b -> b)
+rlr (ab, ba) = ab . ba
+
+str :: String
+str = "JoJo"
+
+pa :: ISO
+  (Either (Either Int Bool) String)
+  (Either Int (Either Bool String))
+pa = plusAssoc
+
 spec = do
   describe "subst" $ do
     it "substL" $ do
@@ -29,3 +40,12 @@ spec = do
     it "lrl isoEU (Left (replicate n ())) == Left (replicate n ())" $
       property $ \(NonNegative n) -> 
         lrl isoEU (Left (replicate n ())) == Left (replicate n ())
+    
+  describe "isoLaw" $ do
+    it "assoc" $ do
+      lrl pa (Left  (Left     0)) `shouldBe` Left  (Left     0)
+      lrl pa (Left  (Right True)) `shouldBe` Left  (Right True)
+      lrl pa (Right          str) `shouldBe` Right          str
+      rlr pa (Left             0) `shouldBe` Left             0
+      rlr pa (Right (Left  True)) `shouldBe` Right (Left  True)
+      rlr pa (Right (Right  str)) `shouldBe` Right (Right  str)
